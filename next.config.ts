0e1c969f0e6ca2811aac49e18a97093604a31806
Arthur Kabera <arthurkabera@gmail.com>
2025-03-02 04:05:18 +0300

    Online api test

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,8 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/backend/:path*",  // Match any request to `/backend/*`
-        destination: "http://localhost:5000/:path*", // Proxy to your backend
+        // destination: "http://localhost:5000/:path*", // Proxy to your backend
+        destination: "http://164.92.79.133:5000/:path*", // Proxy to your backend
       },
     ];
   },
