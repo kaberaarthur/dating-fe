f50bd8ac227abd27176381c2e64236e5ea7b7aec
socialpendo <socialpendo@gmail.com>
2025-03-02 04:43:42 +0300

    solved photouploadmany/one probem

---
+++ src/app/photouploadmany/page.tsx
@@ -0,0 +1,11 @@
+const PhotoUploadMany = () => {
+    return (
+      <div>
+        <h1>Photo Upload Many</h1>
+        <p>This is a simple Next.js page for uploading multiple photos.</p>
+      </div>
+    );
+  };
+  
+  export default PhotoUploadMany;
+  
---
+++ src/app/photouploadone/page.tsx
@@ -0,0 +1,11 @@
+const PhotoUploadOne = () => {
+    return (
+      <div>
+        <h1>Photo Upload One</h1>
+        <p>This is a simple Next.js page for uploading one photo.</p>
+      </div>
+    );
+  };
+  
+  export default PhotoUploadOne;
+  
